1aa0e2f10dfe3139297a40d59262abdf29717ad7
Paul Spooren <paul@spooren.de>
2017-05-22 21:40:05 +0200

    image.py uses imagebuilder

--- update-server/image.py
+++ update-server/image.py
@@ -1,49 +1,64 @@
 import logging
+import hashlib
+import os
+import os.path
 import subprocess
 
-logging.basicConfig(filename="output.log")
+#logging.basicConfig(filename="output.log")
+logging.basicConfig(level=logging.DEBUG)
 
-class Image(distro, version, target, subtarget, model, profile, packages:
-    distro = ""
-    version = ""
-    target = ""
-    subtarget = ""
-    profile = ""
-    model = ""
-    packages = []
-    pkgHash = ""
-    imageName = model + "-" + pkgHash +  ".img"
-    imagePath = os.path.join(distro, version, target, subtarget, imgName)
+
+class Image():
+    def __init__(self, distro, version, target, subtarget, profile, packages):
+        self.distro = distro
+        self.version = version
+        self.target = target
+        self.subtarget = subtarget
+        self.profile = profile
+        self.packages = packages
+        self.pkgHash = self.getPkgHash()
+        self.name = self.profile + "-" + self.pkgHash +  ".img"
+        self.name = "-".join([distro, version, self.pkgHash, target, subtarget, profile, "squashfs", "sysupgrade.bin"])
+        self.path = os.path.join("download", self.distro, self.version, self.target, self.subtarget, self.name)
    
     # returns the path of the created image
-    def get():
+    def get(self):
         if not self.created():
+            logging.info("start build")	
             self.build() 
-
+        else:
+            print("Heureka!")
         return self.path
 
 
     # generate a hash of the installed packages
-    def getPkgHash():
-       pass 
+    def getPkgHash(self):
+        packagesSorted = sorted(self.packages)
+        h = hashlib.sha256()
+        h.update(bytes(" ".join(packagesSorted), 'utf-8'))
+
+        return h.hexdigest()[:12]
 
     # builds the image with the specific packages at output path
-    def build():
+    def build(self):
         # create image path
-        ibPath = os.path.join("imagebuilder", image.target, image.subtarget)
+        ibPath = os.path.abspath(os.path.join("imagebuilder", self.target, self.subtarget))
 
         logging.info("use imagebuilder at %s", ibPath)
 
-        if not os.path.exists(image.path):
-            logging.info("create image path %s", image.path)
-            os.makedirs(image.path)
+        buildPath = os.path.dirname(self.path)
+        if not os.path.exists(buildPath):
+            logging.info("create self path %s", self.path)
+            os.makedirs(os.path.dirname(buildPath))
 
         cmdline = ["make", "image"]
-        cmdline.append("PROFILE=%s" % image.profile)
-        cmdline.append("PACKAGES=%s" % image.packages)
-        cmdline.append("BIN_DIR=%s" % image.path)
+        cmdline.append("PROFILE=%s" % self.profile)
+        cmdline.append("PACKAGES='%s'" % " ".join(self.packages))
+        cmdline.append("BIN_DIR=%s" % os.path.abspath(buildPath))
+        cmdline.append("EXTRA_IMAGE_NAME=%s" % self.pkgHash)
 
         logging.info("start build: %s", cmdline)
+        print(" ".join(cmdline))
 
         proc = subprocess.Popen(
             cmdline,
@@ -55,10 +70,19 @@ class Image(distro, version, target, subtarget, model, profile, packages:
 
         out, _ = proc.communicate()
         ret = proc.returncode
+        print(ret)
 
     # check if image exists
-    def created():
+    def created(self):
         # created images will be stored in downloads.lede-project.org like paths
         # ./lede/17.01.1/ar71xx/wr841-v1.5-6c7e907d06da.img
         # the package should always be a sysupgrade
-        return os.path.exists(self.imagePath)
+        logging.info("check path %s", self.path)
+        return os.path.exists(self.path)
+
+
+if __name__ == "__main__":
+    packages =  ["vim"]
+    logging.info("started logger")
+    image = Image("lede", "17.01.1", "ar71xx", "generic", "tl-wr841-v11", packages)
+    print(image.get())
