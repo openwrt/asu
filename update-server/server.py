3ed6a70e8e90de5f2208253afd47a9d079516d06
Paul Spooren <paul@spooren.de>
2017-05-22 22:03:39 +0200

    added some files

---
+++ update-server/server.py
@@ -0,0 +1,41 @@
+from flask import Flask
+from flask import request
+from replacementTable import *
+
+app = Flask(__name__)
+CURRENT_RELEASE = "17.01.1"
+
+# returns the current release
+# TODO: this won't be static later
+@app.route("/current-release")
+def currentRelease():
+    return CURRENT_RELEASE
+
+# direct link to download a specific image based on hash
+@app.route("/download/<imageHash>")
+def downloadImage(imageHash):
+    # offer file to download
+    pass
+
+# request methos for individual image
+# uses post methos to receive build information
+
+# the post request should contain the following entries
+# distribution, version, revision, target, packages
+@app.route("/request-image", methods=['GET', 'POST'])
+def requstImage():
+    if request.method == 'POST':
+        return(request.form['release'])
+        #return send_from_directory(directory=uploads, filename=filename)
+    else:
+        return(request.args.get('release'))
+    pass
+
+
+# foobar
+@app.route("/")
+def rootPath():
+    return "update server running"
+
+if __name__ == "__main__":
+    app.run()
