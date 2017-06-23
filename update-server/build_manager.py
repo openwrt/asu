09c1ecc32f21c4b31f1cfda7d0bcf244cb5eecdf
Paul Spooren <paul@spooren.de>
2017-06-23 20:20:56 +0200

    build_manager queue still broken

--- update-server/build_manager.py
+++ update-server/build_manager.py
@@ -14,32 +14,19 @@ class BuildManager(threading.Thread):
         self.database = Database()
         self.last_build_id = 1
 
-    def open_socket(self):
-        socket_name = "/tmp/build_manager_last_build_id"
-        if os.path.exists(socket_name):
-            os.remove(socket_name)
-        server = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-        server.bind(socket_name)
-        server.listen()
-
-        while True:
-            print("wait for connection - last_build_id: {}".format(self.last_build_id))
-            connection, client_address = server.accept()
-            try:
-                connection.send(str(self.last_build_id).encode())
-            finally:
-                connection.close()
+    def get_last_build_id(self):
+        print("foobar", self.last_build_id)
+        return self.last_build_id
 
     def run(self):
-        self.last_build_id = 0
         while True:
             build_job_request = self.database.get_build_job()
-            if build_job_request:
-                self.last_build_id = build_job_request[0]
+            print(build_job_request)
             if not build_job_request:
                 self.log.debug("build queue is empty")
                 time.sleep(5)
             else:
+                self.last_build_id = build_job_request[0]
                 image = Image(*build_job_request[2:8])
                 if not image.created():
                     if image.run():
@@ -52,5 +39,18 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     bm = BuildManager()
     bm.start()
-    bm.open_socket()
 
+    socket_name = "/tmp/build_manager_last_build_id"
+    if os.path.exists(socket_name):
+        os.remove(socket_name)
+    server = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+    server.bind(socket_name)
+    server.listen()
+
+    while True:
+        print("wait for connection - last_build_id: {}".format(bm.get_last_build_id()))
+        connection, client_address = server.accept()
+        try:
+            connection.send(str(bm.get_last_build_id()).encode())
+        finally:
+            connection.close()
