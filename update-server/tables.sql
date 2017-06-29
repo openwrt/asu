acfe59f3f1a7ad88233604336158aa9797d678cc
Paul Spooren <paul@spooren.de>
2017-06-29 17:58:54 +0200

    first try on sql functions/views

--- update-server/tables.sql
+++ update-server/tables.sql
@@ -1,15 +1,20 @@
+create table packages_names (
+    id SERIAL PRIMARY KEY,
+	name varchar(20) UNIQUE
+);
+
 create table releases (
-    distro text,
-    release text,
+    distro varchar(20),
+    release varchar(20),
     PRIMARY KEY (distro, release)
 );
 
 create table targets (
     id SERIAL PRIMARY KEY,
-    distro text,
-    release text,
-    target text,
-    subtarget text,
+    distro varchar(20),
+    release varchar(20),
+    target varchar(20),
+    subtarget varchar(20),
 	supported bool DEFAULT false,
     PRIMARY KEY (distro, release, target, subtarget),
     FOREIGN KEY (distro, release) REFERENCES releases
@@ -17,12 +22,12 @@ create table targets (
 
 create table profiles (
     id SERIAL PRIMARY KEY,
-    distro text,
-    release text,
-    target text,
-    subtarget text,
-    name text,
-    board text,
+    distro varchar(20),
+    release varchar(20),
+    target varchar(20),
+    subtarget varchar(20),
+    name varchar(20),
+    board varchar(20),
     PRIMARY KEY(distro, release, target, subtarget, name, board),
     FOREIGN KEY (distro, release, target, subtarget) REFERENCES targets
 );
@@ -30,7 +35,7 @@ create table profiles (
 create table target_packages (
 	profile INTEGER,
 	package INTEGER
-	version text,
+	version varchar(20),
     FOREIGN KEY (profile) REFERENCES targets(id),
     FOREIGN KEY (package) REFERENCES packages_names(id)
 );
@@ -50,34 +55,58 @@ create table default_packages (
 );
 
 create table packages_hashes (
-	hash text primary key,
+	hash varchar(20) primary key,
     package INTEGER
     FOREIGN KEY (package) REFERENCES packages_names(id)
 );
 
-create table packages_names (
-    id SERIAL PRIMARY KEY,
-	name text UNIQUE
-);
 
 
 create table images (
     id SERIAL PRIMARY KEY,
-    image_hash text UNIQUE,
-    distro text,
-    release text,
-    target text,
-    subtarget text,
-    profile text,
-    package_hash text,
-    network_profile text,
-    checksum text,
+    image_hash varchar(20) UNIQUE,
+    distro varchar(20),
+    release varchar(20),
+    target varchar(20),
+    subtarget varchar(20),
+    profile varchar(20),
+    package_hash varchar(20),
+    network_profile varchar(20),
+    checksum varchar(20),
 	filesize integer,
 	build_date timestamp,
 	last_download timestamp,
 	downloads integer DEFAULT 0,
 	keep boolean DEFAULT false,
-    status text DEFAULT 'requested',
+    status varchar(20) DEFAULT 'requested',
     FOREIGN KEY (distro, release, target, subtarget) REFERENCES targets,
     FOREIGN KEY (package_hash) REFERENCES packages_hashes(hash)
 );
+
+create function add_default_packages(distro varchar(20), release varchar(20), target varchar(20), subtarget varchar(20), packages text) returns void as
+$$
+declare
+	packages_array = string_to_array(packages, ' ')
+begin
+	for package in packages_array
+	loop
+		insert package into packages_names where not in (select name from packages_names);
+		insert into default_packages values(
+			(select id from targets where 
+				targets.distro = distro,
+				targets.release = release,
+				targets.target = target,
+				targets.subtarget = subtarget), 
+			(select id from packages where packages.name = package)
+		);
+	end loop;
+end
+$$ language 'plpgsql';
+
+create view get_default_packages as
+	select packages.name
+	from packages, targets, default_packages
+	where 
+		default_packages.package = packages.id,
+		default_packages.target = targets.id
+	;
