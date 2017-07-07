47c72bcdbf960c4c17f3154d75846bf59c0b7dbc
Paul Spooren <paul@spooren.de>
2017-07-07 20:32:19 +0200

    make config usable for different files

--- update-server/config.py
+++ update-server/config.py
@@ -3,14 +3,17 @@ import os.path
 from shutil import copyfile
 
 class Config():
-    def __init__(self):
+    def __init__(self, config_file="config.yml"):
         self.config = {}
 
-        if not os.path.exists("config.yml"):
-            copyfile("config.yml.default", "config.yml")
+        if not os.path.exists(config_file):
+            copyfile((config_file + ".default"), config_file)
             
-        with open("config.yml", 'r') as ymlfile:
+        with open(config_file, 'r') as ymlfile:
             self.config = yaml.load(ymlfile)
+
+    def get_all(self):
+        return self.config
 
     def get(self, option):
         if option in self.config:
