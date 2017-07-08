7012db80690f8171e078802a50df3133b0b62c6d
Paul Spooren <paul@spooren.de>
2017-07-08 20:16:30 +0200

    updated demos

--- demo/testimage.sh
+++ demo/testimage.sh
@@ -1,5 +1,5 @@
 # sends a request to the demo update server
-UPDATESERVER="35.189.253.152"
-#UPDATESERVER="localhost:5000"
+UPDATESERVER="localhost:5000"
+UPDATESERVER="https://betaupdate.libremesh.org"
 
 curl -w "\nStatuscode: %{http_code}\n" -X POST "$UPDATESERVER"/image-request -d @testimage.json --header "Content-Type: application/json"
