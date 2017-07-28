cf225eb1d1858375f23c41826045aa263e377d50
Paul Spooren <paul@spooren.de>
2017-07-28 15:50:24 +0200

    move luci-app to luci.git
    moved to official openwrt/luci.git
    currently PR pending:
    https://github.com/openwrt/luci/pull/1254
    
    Signed-off-by: Paul Spooren <paul@spooren.de>

--- package/luci-app-attended-sysupgrade/files/attended-sysupgrade.lua
+++
@@ -1,5 +0,0 @@
-module("luci.controller.attended-sysupgrade", package.seeall)
-
-function index()
-        entry({"admin", "system", "attended_sysupgrade"}, template("attended-sysupgrade"), _("Attended Sysupgrade"), 1)
-end
