0c2a79b91e2a52a9035f096a380701944c8d5927
Paul Spooren <paul@spooren.de>
2017-07-31 18:28:49 +0200

    remove outdated tests

--- update-server/tests/test_image.sh
+++
@@ -1,5 +0,0 @@
-# sends a request to the running update server
-UPDATESERVER="localhost:5000"
-
-curl -vX POST "$UPDATESERVER"/image-request -d @test_image.json --header "Content-Type: application/json"
-curl -vX POST "$UPDATESERVER"/image-request -d @test_image2.json --header "Content-Type: application/json"
