7852c0ffc0547009f70c14b01369e32171556888
Paul Spooren <paul@spooren.de>
2017-08-11 20:07:33 +0200

    improve package updates
    the client sends all installed packages to the server, the server
    transforms all installed packages and checks for updates. returns a list
    of packages "packages"; a list of transformed packages "transformation"
    and a list of updated packages "updates".

--- update-server/update_request.py
+++ update-server/update_request.py
@@ -9,27 +9,41 @@ class UpdateRequest(Request):
         self.log = logging.getLogger(__name__)
         self.needed_values = ["distro", "version", "target", "subtarget"]
 
+    def package_transformation(self, distro, release, packages):
+        # perform package transformation
+        # this function is still a dummy
+        self.packages_transformed = {}
+        if self.packages_transformed:
+            self.response_dict["transformations"] = self.packages_transformed
+        return packages
+
     def run(self):
         bad_request = self.check_bad_request()
         if bad_request:
             return bad_request
 
         self.latest_release = get_latest_release(self.distro)
+
         if self.release  == "snapshot":
             self.response_dict["version"] = "SNAPSHOT"
-            return self.respond(), HTTPStatus.OK
         elif not self.release_latest(self.latest_release, self.release):
             self.response_dict["version"] = self.latest_release
-            return self.respond(), HTTPStatus.OK
 
         if "packages" in self.request_json:
-            # perform package transformation
-            response = self.database.packages_updates(self.distro, self.release, self.target, self.subtarget, self.request_json["packages"])
-            if response:
-                self.response_dict["packages"] = {}
-                for name, version, version_installed in response:
-                    self.response_dict["packages"][name] = [version, version_installed]
+            self.packages_installed = self.package_transformation(self.distro, self.release, self.request_json["packages"])
 
-                return self.respond(), HTTPStatus.OK
+            packages_updates = self.database.packages_updates(self.distro, self.release, self.target, self.subtarget, self.packages_installed)
+            if packages_updates:
+                self.response_dict["updates"] = {}
+                for name, version, version_installed in packages_updates:
+                    self.response_dict["updates"][name] = [version, version_installed]
 
-        return("", HTTPStatus.NO_CONTENT)
+            self.response_dict["packages"] = []
+            self.response_dict["packages"].extend(list(self.packages_installed.keys()))
+            self.response_dict["packages"].extend(list(self.packages_transformed.keys()))
+            self.log.warning(self.response_dict["packages"])
+
+        if "version" in self.response_dict or "packages" in self.response_dict:
+            return(self.respond(), HTTPStatus.OK)
+        else:
+            return("", HTTPStatus.NO_CONTENT)
