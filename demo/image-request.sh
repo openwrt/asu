8e5c67a643870bb95fec4ffa99712440c6d1694d
Paul Spooren <paul@spooren.de>
2017-08-19 15:13:26 +0200

    improve demo script
    replace libremesh with lime

--- demo/image-request.sh
+++ demo/image-request.sh
@@ -1,5 +1,33 @@
 # sends a request to the demo update server
 UPDATESERVER="localhost:5000"
 UPDATESERVER="https://betaupdate.libremesh.org"
+JSON_FILE=$1
 
-curl -w "\nStatuscode: %{http_code}\n" -X POST "$UPDATESERVER"/image-request -d @$1 --header "Content-Type: application/json"
+function image_request () {
+	response=$(curl -s -w "\n%{http_code}" -X POST "$UPDATESERVER"/image-request -d @$JSON_FILE --header "Content-Type: application/json")
+	export statuscode=$(echo "$response" | tail -n 1)
+	export content=$(echo "$response" | head -n 1)
+}
+
+image_request
+
+while [ "$statuscode" -ne 200 ];  do
+	if [ $statuscode -eq 500 ]; then
+		echo "internal server error"
+		echo "$content"
+		exit 1
+	elif [ $statuscode -eq 400 ]; then
+		echo "bad request"
+		echo "$content"
+		exit 1
+	elif [ $statuscode -eq 201 ]; then
+			echo "setting up imagebuilder - please wait"
+	elif [ $statuscode -eq 206 ]; then
+			echo "currently building - please wait"
+	fi
+	image_request
+	sleep 5
+done
+
+echo "build successfull"
+echo $content
