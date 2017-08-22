42043aaeba4b76e91957f45c0a928218b6def21c
Paul Spooren <paul@spooren.de>
2017-08-22 02:25:54 +0200

    replacement table python part 1

--- update-server/replacement_table.py
+++ update-server/replacement_table.py
@@ -1,4 +1,7 @@
 import json
+import yaml
+from config import Config
+import os
 
 # this function will return the package names changed due to the replacement table
 # distro: installed distribution
@@ -13,10 +16,42 @@ def check_packages(distro, installedRelease, packages):
     # this will contain choices to offer in the user interface
     # this is only needed if the dependencie tree can't provide a default answer
     response["choices"] = []
-    #response["choices"].append(("bigPkg", ["bigPkg-light", "bigPkg-full"])) 
+    #response["choices"].append(("bigPkg", ["bigPkg-light", "bigPkg-full"]))
 
     return response
-
 # this function will load all replacement tables
 def load_tables():
-    pass
+    config = Config()
+    distros = {}
+    for distro in config.get("distributions").keys():
+        distros[distro] = {}
+        releases = yaml.load(open(os.path.join("distributions", distro, "releases.yml")).read())
+        for release in releases:
+            release = str(release)
+            release_replacements_path = os.path.join("distributions", distro, (release + ".yml"))
+            if os.path.exists(release_replacements_path):
+                with open(release_replacements_path, "r") as release_replacements_file:
+                    replacements = yaml.load(release_replacements_file.read())
+                    if replacements:
+                        if "transformations" in replacements:
+                            insert_replacements(distro, release, replacements["transformations"])
+
+def insert_replacements(distro, release, transformations):
+    for package, action in transformations.items():
+        if not action:
+            # drop package
+            print("drop", package)
+        elif type(action) is str:
+            # replace
+            print("replace", package, "with", action)
+        elif type(action) is dict:
+            for key, value in action.items():
+                if value is True:
+                    print("default", key)
+                elif value is False:
+                    print("choice", key)
+                elif type(value) is list:
+                    for dependencie in value:
+                        print("dependencie", dependencie, "for", key)
+
+load_tables()
