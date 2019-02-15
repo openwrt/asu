48557753e3ed85bc183d3aa29f526ca3c8b69eaf
Paul Spooren <mail@aparcar.org>
2019-02-15 19:32:47 +0100

    add random tests
    these should later become real unit tests

--- tests/image_req.sh
+++ tests/image_req.sh
@@ -1,7 +1,6 @@
 SERVER="https://as-test.stephen304.com"
 SERVER="http://localhost:5000"
 
-curl  --request POST -H "Content-Type: application/json" --data @image.json "$SERVER"/api/build-request
-echo
-curl  --request POST -H "Content-Type: application/json" --data @image2.json "$SERVER"/api/build-request
-echo
+curl -i  --request POST -H "Content-Type: application/json" --data @image.json "$SERVER"/api/upgrade-request
+#curl  --request POST -H "Content-Type: application/json" --data @image.json "$SERVER"/api/build-request
+#curl  --request POST -H "Content-Type: application/json" --data @image2.json "$SERVER"/api/upgrade-request
