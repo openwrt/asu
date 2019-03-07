5d4ba5d9089e843b3ebb8664ccb68b1e07566af5
Paul Spooren <mail@aparcar.org>
2019-03-07 19:21:14 +0100

    pypi: add tables.sql

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with io.open("README.md", "rt", encoding="utf8") as f:
 
 setup(
     name="asu",
-    version="0.1",
+    version="0.2.0",
     url="https://github.com/aparcar/attendedsysupgrade-server",
     license="",
     maintainer="Paul Spooren",
