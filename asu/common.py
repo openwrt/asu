7d9998adb947edaf6cc20afdb7a19f4e328a95f8
Paul Spooren <mail@aparcar.org>
2020-02-29 14:29:37 -1000

    Add initial documentation via `mkdocs`
    Signed-off-by: Paul Spooren <mail@aparcar.org>

--- asu/common.py
+++ asu/common.py
@@ -1,10 +1,20 @@
+from pathlib import Path
+import base64
 import hashlib
 import nacl.signing
 import struct
-import base64
 
 
-def get_str_hash(string: str, length: int) -> str:
+def get_str_hash(string: str, length: int = 32) -> str:
+    """Return sha256sum of str with optional length
+
+    Args:
+        string (str): input string
+        length (int): hash length
+
+    Returns:
+        str: hash of string with specified length
+    """
     h = hashlib.sha256()
     h.update(bytes(string, "utf-8"))
     response_hash = h.hexdigest()[:length]
@@ -12,6 +22,14 @@ def get_str_hash(string: str, length: int) -> str:
 
 
 def get_file_hash(path: str) -> str:
+    """Return sha256sum of given path
+
+    Args:
+        path (str): path to file
+
+    Returns:
+        str: hash of file
+    """
     BLOCK_SIZE = 65536
 
     h = hashlib.sha256()
@@ -24,7 +42,17 @@ def get_file_hash(path: str) -> str:
     return h.hexdigest()
 
 
-def get_request_hash(request_data):
+def get_request_hash(request_data: dict) -> str:
+    """Return sha256sum of an image request
+
+    Creates a reproducible hash of the request by sorting the arguments
+
+    Args:
+        request_data (dict): dict contianing request information
+
+    Returns:
+        str: hash of `request_data`
+    """
     request_data["packages_hash"] = get_packages_hash(request_data.get("packages", ""))
     request_array = [
         request_data.get("distro", ""),
@@ -37,10 +65,37 @@ def get_request_hash(request_data):
 
 
 def get_packages_hash(packages: list) -> str:
+    """Return sha256sum of package list
+
+    Duplicate packages are automatically removed and the list is sorted to be
+    reproducible
+
+    Args:
+        packages (list ): list of packages
+
+    Returns:
+        str: hash of `request_data`
+    """
     return get_str_hash(" ".join(sorted(list(set(packages)))), 12)
 
 
-def verify_usign(sig_file, msg_file, pub_key: str) -> bool:
+def verify_usign(sig_file: Path, msg_file: Path, pub_key: str) -> bool:
+    """Verify a signify/usign signature
+
+    This implementation uses pynacl
+
+    Args:
+        sig_file (Path): signature file
+        msg_file (Path): message file to be verified
+        pub_key (str): public key to use for verification
+
+    Returns:
+        bool: Sucessfull verification
+
+    Todo:
+         Currently ignores keynum and pkalg
+
+    """
     pkalg, keynum, pubkey = struct.unpack("!2s8s32s", base64.b64decode(pub_key))
     sig = base64.b64decode(sig_file.read_text().splitlines()[-1])
 
