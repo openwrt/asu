afcfb4b7b6cf40d4a4daab5c4e0420cedf8c1f6b
Paul Spooren <mail@aparcar.org>
2020-03-19 20:08:22 -1000

    build: implement `diff_packages`
    When activated default packages are not installed but only the packages
    actually requested. This is different from web interfaces where default
    plus additional packages are installed. This function is usfull when
    upgrading a running device which sends all installed packages to the
    server.
    
    Signed-off-by: Paul Spooren <mail@aparcar.org>

--- tests/test_build.py
+++ tests/test_build.py
@@ -6,7 +6,8 @@ import pytest
 from pytest_httpserver import HTTPServer
 
 
-def test_build_fake(app, httpserver: HTTPServer):
+@pytest.fixture
+def upstream(httpserver):
     base_url = "/snapshots/targets/testtarget/testsubtarget"
     upstream_path = Path("./tests/upstream/snapshots/targets/testtarget/testsubtarget/")
     expected_file_requests = [
@@ -17,8 +18,90 @@ def test_build_fake(app, httpserver: HTTPServer):
 
     for f in expected_file_requests:
         httpserver.expect_request(f"{base_url}/{f}").respond_with_data(
-            (upstream_path / f).read_bytes()
+            (upstream_path / f).read_bytes(),
+            headers={"Last-Modified": "Thu, 19 Mar 2020 20:27:41 GMT"},
         )
+
+
+def test_build_fake(app, upstream):
+    request_data = dict(
+        version_data={
+            "branch": "master",
+            "path": "snapshots",
+            "pubkey": "RWSrHfFmlHslUcLbXFIRp+eEikWF9z1N77IJiX5Bt/nJd1a/x+L+SU89",
+        },
+        target="testtarget/testsubtarget",
+        store_path=app.config["STORE_PATH"],
+        cache_path=app.config["CACHE_PATH"],
+        upstream_url="http://localhost:8001",
+        version="SNAPSHOT",
+        profile="testprofile",
+        packages={"test1", "test2"},
+    )
+    result = build(request_data)
+    assert result["id"] == "testprofile"
+
+
+def test_build_fake_diff_packages(app, upstream):
+    request_data = dict(
+        version_data={
+            "branch": "master",
+            "path": "snapshots",
+            "pubkey": "RWSrHfFmlHslUcLbXFIRp+eEikWF9z1N77IJiX5Bt/nJd1a/x+L+SU89",
+        },
+        target="testtarget/testsubtarget",
+        store_path=app.config["STORE_PATH"],
+        cache_path=app.config["CACHE_PATH"],
+        upstream_url="http://localhost:8001",
+        version="SNAPSHOT",
+        profile="testprofile",
+        packages={"test1", "test2"},
+        diff_packages=True,
+    )
+    result = build(request_data)
+    assert result["id"] == "testprofile"
+
+
+def test_build_fake_no_packages(app, upstream):
+    request_data = dict(
+        version_data={
+            "branch": "master",
+            "path": "snapshots",
+            "pubkey": "RWSrHfFmlHslUcLbXFIRp+eEikWF9z1N77IJiX5Bt/nJd1a/x+L+SU89",
+        },
+        target="testtarget/testsubtarget",
+        store_path=app.config["STORE_PATH"],
+        cache_path=app.config["CACHE_PATH"],
+        upstream_url="http://localhost:8001",
+        version="SNAPSHOT",
+        profile="testprofile",
+    )
+    result = build(request_data)
+    assert result["id"] == "testprofile"
+
+
+def test_build_fake_list_packages(app, upstream):
+    request_data = dict(
+        version_data={
+            "branch": "master",
+            "path": "snapshots",
+            "pubkey": "RWSrHfFmlHslUcLbXFIRp+eEikWF9z1N77IJiX5Bt/nJd1a/x+L+SU89",
+        },
+        target="testtarget/testsubtarget",
+        store_path=app.config["STORE_PATH"],
+        cache_path=app.config["CACHE_PATH"],
+        upstream_url="http://localhost:8001",
+        version="SNAPSHOT",
+        profile="testprofile",
+        packages=["test1"],
+    )
+    with pytest.raises(Exception) as execinfo:
+        result = build(request_data)
+    assert str(execinfo.value) == "packages must be type set not list"
+
+
+def test_build_fake_store_path_not_exists(app, upstream):
+    app.config["STORE_PATH"].rmdir()
 
     request_data = dict(
         version_data={
@@ -32,10 +115,10 @@ def test_build_fake(app, httpserver: HTTPServer):
         upstream_url="http://localhost:8001",
         version="SNAPSHOT",
         profile="testprofile",
-        packages=["test1", "test2"],
     )
-    result = build(request_data)
-    assert result["id"] == "testprofile"
+    with pytest.raises(Exception) as execinfo:
+        result = build(request_data)
+    assert str(execinfo.value) == "store_path must be existing directory"
 
 
 @pytest.mark.slow
@@ -52,7 +135,7 @@ def test_build_real(app, httpserver: HTTPServer):
         upstream_url="https://cdn.openwrt.org",
         version="SNAPSHOT",
         profile="tplink_tl-wdr4300-v1",
-        packages=["tmux", "vim"],
+        packages={"tmux", "vim"},
     )
     result = build(request_data)
     assert result["id"] == "tplink_tl-wdr4300-v1"
