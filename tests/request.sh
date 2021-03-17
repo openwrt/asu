3c5a7d9e63410cfa6d4c5b7acc2fa14bd45cd9ea
Paul Spooren <mail@aparcar.org>
2021-03-17 10:54:02 -1000

    tests: remove accidentally added test scripts
    Signed-off-by: Paul Spooren <mail@aparcar.org>

--- tests/request.sh
+++
@@ -1,8 +0,0 @@
-curl --header "Content-Type: application/json" \
-	--request POST \
-	--data @request.json \
-	http://localhost:5000/api/build
-curl --header "Content-Type: application/json" \
-	--request POST \
-	--data @request.json \
-	https://chef.libremesh.org/api/build
