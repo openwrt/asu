f13d7dc55d520aebef3bfba861375138055e0361
Paul Spooren <mail@aparcar.org>
2023-02-04 23:29:15 +0100

    chore: document config.py options
    Fix: #398
    
    Signed-off-by: Paul Spooren <mail@aparcar.org>

--- misc/config.py
+++ misc/config.py
@@ -16,4 +16,23 @@ CACHE_PATH = None
 # where to store JSON files
 JSON_PATH = Path.cwd() / "public/json/v1/"
 
+# manual mapping of package ABI changes
 MAPPING_ABI = {"libubus20191227": "libubus"}
+
+# connection string for Redis
+# REDIS_CONN = Redis(host=redis_host, port=redis_port, password=redis_password)
+
+# run jobs in worker processes or on the server (for testing)
+# ASYNC_QUEUE = True
+
+# allow users to add a boot script to the images
+# ALLOW_DEFAULTS = False
+
+# definition of branches, see and use branches.yml instead (unless testing)
+# BRANCHES = {}
+
+# what branches.yml file to load
+# BRANCHES_FILE = "./branches.yml"
+
+# where to downlaod the images from
+# UPSTREAM_PATH = "https://downloads.openwrt.org"
