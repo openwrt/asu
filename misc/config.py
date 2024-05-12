808f53c465a8b742af6076e158102f25cf4cae8f
Eric Fahlgren <ericfahlgren@gmail.com>
2024-05-11 17:22:14 -0700

    build: increase MAX_CUSTOM_ROOTFS_SIZE_MB to a reasonable value
    Current value of 100 is less than default when building OpenWrt (104).
    Increase it to 1024 to allow large root file systems on devices that
    can accommodate them.
    
    Closes: https://github.com/openwrt/asu/issues/826
    
    Signed-off-by: Eric Fahlgren <ericfahlgren@gmail.com>

--- misc/config.py
+++ misc/config.py
@@ -14,7 +14,7 @@ STORE_PATH = Path.cwd() / "public/store/"
 JSON_PATH = Path.cwd() / "public/json/v1/"
 
 # maximum custom rootfs size
-MAX_CUSTOM_ROOTFS_SIZE_MB = 100
+MAX_CUSTOM_ROOTFS_SIZE_MB = 1024
 
 # manual mapping of package ABI changes
 MAPPING_ABI = {"libubus20191227": "libubus"}
