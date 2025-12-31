69af40590b4fec70fce676a5e5305aa1f3f8d750
Hauke Mehrtens <hauke@hauke-m.de>
2025-12-31 17:42:48 +0100

    package_changes.py: 25.12 Add kmod-dsa-mv88e6xxx
    The mv88e6xxx dsa driver is not compiled into the kernel any more. Add
    the package to the targets needing it.
    
    This was changed in OpenWrt in:
    https://github.com/openwrt/openwrt/commit/eaa82118eadfd495f8512d55c01c1935b8b42c51
    https://github.com/openwrt/openwrt/commit/8a7239009c5f4b28b696042b70ed1f8f89902915
    
    Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

--- asu/package_changes.py
+++ asu/package_changes.py
@@ -59,6 +59,62 @@ def apply_package_changes(build_request: BuildRequest):
             build_request.packages.remove("auc")
             _add_if_missing("owut")
 
+    # 25.12 specific changes
+    if build_request.version.startswith("25.12"):
+        # Changes for https://github.com/openwrt/openwrt/commit/8a7239009c5f4b28b696042b70ed1f8f89902915
+        if build_request.target == "kirkwood/generic":
+            if build_request.profile in {
+                "checkpoint_l-50",
+                "endian_4i-edge-200",
+                "linksys_e4200-v2",
+                "linksys_ea3500",
+                "linksys_ea4500",
+            }:
+                _add_if_missing("kmod-dsa-mv88e6xxx")
+        # Changes for https://github.com/openwrt/openwrt/commit/eaa82118eadfd495f8512d55c01c1935b8b42c51
+        elif build_request.target == "mvebu/cortexa9":
+            if build_request.profile in {
+                "cznic_turris-omnia",
+                "fortinet_fg-30e",
+                "fortinet_fwf-30e",
+                "fortinet_fg-50e",
+                "fortinet_fg-51e",
+                "fortinet_fg-52e",
+                "fortinet_fwf-50e-2r",
+                "fortinet_fwf-51e",
+                "iij_sa-w2",
+                "linksys_wrt1200ac",
+                "linksys_wrt1900acs",
+                "linksys_wrt1900ac-v1",
+                "linksys_wrt1900ac-v2",
+                "linksys_wrt3200acm",
+                "linksys_wrt32x",
+                "marvell_a370-rd",
+            }:
+                _add_if_missing("kmod-dsa-mv88e6xxx")
+        # Changes for https://github.com/openwrt/openwrt/commit/eaa82118eadfd495f8512d55c01c1935b8b42c51
+        elif build_request.target == "mvebu/cortexa53":
+            if build_request.profile in {
+                "glinet_gl-mv1000",
+                "globalscale_espressobin",
+                "globalscale_espressobin-emmc",
+                "globalscale_espressobin-ultra",
+                "globalscale_espressobin-v7",
+                "globalscale_espressobin-v7-emmc",
+                "methode_udpu",
+            }:
+                _add_if_missing("kmod-dsa-mv88e6xxx")
+        # Changes for https://github.com/openwrt/openwrt/commit/eaa82118eadfd495f8512d55c01c1935b8b42c51
+        elif build_request.target == "mvebu/cortexa72":
+            if build_request.profile in {
+                "checkpoint_v-80",
+                "checkpoint_v-81",
+                "globalscale_mochabin",
+                "mikrotik_rb5009",
+                "solidrun_clearfog-pro",
+            }:
+                _add_if_missing("kmod-dsa-mv88e6xxx")
+
     # TODO: if we ever fully implement 'packages_versions', this needs rework
     for version, packages in language_packs.items():
         if build_request.version >= version:  # Includes snapshots
