4092578a26cb0cda4d217928d42bc758c3c1c93c
Eric Fahlgren <ericfahlgren@gmail.com>
2026-03-26 09:13:53 -0700

    build: improve package error conflict parsing
    Improve apk conflicts message parsing to handle case where
    conflicting file ownership is detected.
    
    Signed-off-by: Eric Fahlgren <ericfahlgren@gmail.com>

--- tests/test_util.py
+++ tests/test_util.py
@@ -340,8 +340,9 @@ def test_check_package_errors():
         check_package_errors(check_package_errors.__doc__)
         == "Impossible package selection:"
         " missing (APK-MISSING, OPKG-MISSING)"
-        " conflicts (APK-CONFLICT-1, APK-CONFLICT-2, OPKG-CONFLICT-1,"
-        " OPKG-CONFLICT-2, OPKG-CONFLICT-3, OPKG-CONFLICT-4)"
+        " conflicts"
+        " (APK-CONFLICT-1, APK-CONFLICT-2, APK-CONFLICT-3, APK-CONFLICT-4,"
+        " OPKG-CONFLICT-1, OPKG-CONFLICT-2, OPKG-CONFLICT-3, OPKG-CONFLICT-4)"
     )
 
 
